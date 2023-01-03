1147c8cecc9577521ff5da90a75bf6b5c5c88f3f
ctbishop <121835797+ctbishop@users.noreply.github.com>
2023-01-02 16:37:38 -1000

    Update easymon-uninstall.sh

--- easymon-uninstall.sh
+++ easymon-uninstall.sh
@@ -5,13 +5,13 @@
 ##########################################################################################
 #  Deinstallation script for "EasyMon" System Monitor app.                               #
 #                                                                                        #
-#     Author Contact: ctbishop34@gmail.com                                               #
+#     Author: C. Bishop
+#     Origin: https://github.com/ctbishop/EasyMon                                        #
 #                                                                                        #
 #     PLEASE NOTE:                                                                       #
 #     Although written with the best intentions, this script is not guaranteed to        #
 #     function as intended or to produce the desired result. The execution of this       #
-#     script is thus at your own risk. Please report bugs and other issues to            #
-#     ctbishop34@gmail.com.                                                              #
+#     script is thus at your own risk.                                                      #
 #                                                                                        #
 ##########################################################################################
 
